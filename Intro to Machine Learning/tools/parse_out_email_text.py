e5fcbb6daf32b234d2fae9d6b2b748a79a886cbe
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-01-22 23:38:08 -0500

    Finish text learning

--- Intro to Machine Learning/tools/parse_out_email_text.py
+++ Intro to Machine Learning/tools/parse_out_email_text.py
@@ -8,12 +8,12 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -25,22 +25,29 @@ def parseOutText(f):
     words = ""
     if len(content) > 1:
         ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        text_string = content[1].translate(string.maketrans("", ""), \
+            string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        ### words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
 
+        text_strings = text_string.split()
 
+        stemmer = SnowballStemmer("english")
+
+        for i, word in enumerate(text_strings):
+            text_strings[i] = stemmer.stem(text_strings[i])
+
+        words = ' '.join(text_strings)
 
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +58,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
