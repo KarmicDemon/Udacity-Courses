7fe43072ad0859fc2f4d4e8df99bad3f19f5eb33
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-01-25 11:38:21 -0500

    Finished the course

--- Intro to Machine Learning/evaluation/evaluate_poi_identifier.py
+++ Intro to Machine Learning/evaluation/evaluate_poi_identifier.py
@@ -14,7 +14,11 @@
 import pickle
 import sys
 sys.path.append("../tools/")
+
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import precision_score, recall_score
+from sklearn.tree import DecisionTreeClassifier
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,8 +28,15 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split\
+    (features, labels, test_size=.3, random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-### your code goes here 
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
 
-
+print 'Prediction Score:', precision_score(predictions, true_labels)
+print 'Recall Score:', recall_score(predictions, true_labels)
